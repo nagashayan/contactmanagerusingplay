7d8ca5a3a9d04feefd260eb3e00908156df45629
Nagashayan <nagashayan1@gmail.com>
2018-02-03 19:22:46 -0500

    add static user authentication system.

--- contactapp/app/controllers/Application.java
+++ contactapp/app/controllers/Application.java
@@ -10,16 +10,32 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
+        String error = "";
+        if(!error.equals("")){
+            error= "User or Password not found";
+        }
+        render(error);
+    }
+    
+    public static void home() {
         String name = "naga";
         render(name);
     }
     
     public static void login() {
-        render("@index");
+        render();
+    }
+    
+    public static void validate(String email, String password) {
+        if(email.equals("naga") && password.equals("naga")){
+            home();
+        }
+        String error = "loginerror";
+        index();
     }
     
     public static void logout() {
-        render();
+        index();
     }
     
     public static void register() {
