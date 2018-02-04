24dea1c9576864fc52f0fbe8844b3e576e8bc0ed
Nagashayan <nagashayan1@gmail.com>
2018-02-03 20:30:01 -0500

    add user model

---
+++ contactapp/app/models/User.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+import javax.persistence.Entity;
+import play.data.validation.Email;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author nagashayanaramamurthy
+ */
+@Entity
+public class User extends Model {
+
+    @Email
+    @Required
+    public String email;
+    
+    @Required
+    public String password;
+    
+}
